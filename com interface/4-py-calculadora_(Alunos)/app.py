cb2f8c9853cdc33536c601d1ef0f0f1995e59aa3
LeandroFernandes12 <leandro.f.santos10@aluno.senai.br>
2025-08-18 10:50:10 -0300

    Finalização do arquivo 5
    Finalizando o arquivo 5-senha e inicio do arquivo 4-calculadora.

---
+++ com interface/4-py-calculadora_(Alunos)/app.py
@@ -0,0 +1,6 @@
+import ttkbootstrap as ttk # Importa a biblioteca ttkbootstrap para criar a interface gráfica
+from ttkbootstrap.constants import * # Importa constantes da biblioteca ttkbootstrap
+from PIL import Image, ImageTk
+from functools import partial
+import os
+import sys
